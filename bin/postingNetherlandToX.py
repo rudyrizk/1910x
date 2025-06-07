1b9d751b2dcf68f5b6c7010a136269974223b864
rudy <31217309+rudyrizk@users.noreply.github.com>
2025-06-07 23:21:00 +0300

    Update postingNetherlandToX.py

--- bin/postingNetherlandToX.py
+++ bin/postingNetherlandToX.py
@@ -118,4 +118,4 @@ if __name__ == "__main__":
     if final_content:
         print(final_content)  # Print the final combined content
         # Post the fetched content to Twitter
-        post_to_twitter(final_content)
+        post_to_twitter_OLD(final_content)
